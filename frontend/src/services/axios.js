cfe056bc6d9c5254eb1275beb4c9f4412130a957
ved galgat <vedgalgat8233@gmail.com>
2025-12-30 17:59:30 -0800

    Update backend API URL for deployment

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://contact-vault-ek24.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
